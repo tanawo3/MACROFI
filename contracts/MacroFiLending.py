a32e3abbf97264da0d2fefd0fd04503f94dac389
Omar <omar@genlayer.com>
2026-07-16 16:44:06 +0100

    Initial commit

--- contracts/MacroFiLending.py
+++ contracts/MacroFiLending.py
@@ -4,7 +4,6 @@ import json
 import re
 from dataclasses import dataclass
 from genlayer import *
-import genlayer as gl
 
 # =============================================================================
 # ENTERPRISE PROTOCOL CONFIGURATION
